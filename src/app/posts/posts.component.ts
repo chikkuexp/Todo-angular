4ea848e2baf95661cd3b7ed550abbce63d1f3261
chikkupa <chikku@hashin.in>
2019-02-05 17:12:28 +0530

    added CRUD on post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -19,10 +19,23 @@ export class PostsComponent implements OnInit {
   createPost(input: HTMLInputElement){
     let post = {title: input.value};
     input.value = "";
-    
+
     this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
       post['id'] = response.json().id;
       this.posts.splice(0, 0, post);
+    });
+  }
+
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id, {isRead: true}).subscribe(response => {
+      console.log(response.json());
+    });
+  }
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id).subscribe(response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
     });
   }
 
